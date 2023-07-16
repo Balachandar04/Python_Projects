51fb5ad8fe60f3f955fb515840e19caa6f6de264
Balachandar <balachandar4010@gmail.com>
2023-07-16 09:29:59 +0530

    created py workspace

---
+++ alogrithms/insertionSort.py
@@ -0,0 +1,16 @@
+def insertSort(arr):
+    for i in range(1,len(arr)):
+        key = arr[i]       # key to store the value to compare with other elements
+        j =  i-1           # j to point current comparing element(with the key) in the array
+        k=i                # k to move comparing pairs (since i cannot be moved )
+        while j>=0 and key<arr[j]:
+            arr[k]=arr[j]
+            j-=1
+            k-=1
+        arr[k]=key
+        print(arr)
+    return arr
+
+arr1= [10,3,6,2,1]
+print(arr1)
+print(insertSort(arr1))
